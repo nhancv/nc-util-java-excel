ac4e1bb70854bae7ba892476700cb6d45a9e9fdd
Nhan Cao <nhancv1992@gmail.com>
2016-05-26 10:39:32 +0700

    initial
    mvn install

---
+++ src/main/java/Home.java
@@ -0,0 +1,11 @@
+/**
+ * Created by nhancao on 5/26/16.
+ */
+public class Home {
+
+
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by nhancao on 5/26/16.
+ */
+public class HomeTest {
+
+}
